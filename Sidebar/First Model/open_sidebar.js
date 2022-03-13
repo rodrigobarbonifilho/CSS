db2f45f8589ebcdd5da6e0a8fd9108b8eb3e0e2d
Rodrigo Barboni Filho <68304421+rodrigobarbonifilho@users.noreply.github.com>
2022-03-13 04:04:57 -0300

    Criando uma página guia para o meu repositório de desafios CSS

--- Sidebar/First Model/open_sidebar.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector(".sidebar__button");
-const sidebar = document.querySelector(".sidebar")
-let isOpen = true
-
-button.addEventListener("click", () => {
-    button.classList.toggle("sidebar__button--open", isOpen);
-    sidebar.classList.toggle("sidebar--open", isOpen)
-    isOpen = !isOpen;
-})
